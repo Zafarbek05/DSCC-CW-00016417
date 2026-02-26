41d2b18d819332b03ba172d1caad3beaf1e6014d
Zafarbek Jabborov <jabborovz2005@gmail.com>
2026-02-26 21:55:56 +0500

    feat: add CI/CD pipeline and tests for category model

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from .models import Category
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_home_page_status():
+    url = reverse('home')
+    assert url == '/'
+
+@pytest.mark.django_db
+def test_login_page_status(client):
+    response = client.get(reverse('login'))
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_register_page_status(client):
+    response = client.get(reverse('register'))
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_category_creation():
+    category = Category.objects.create(name="Study")
+    assert category.name == "Study"
+
+@pytest.mark.django_db
+def test_category_string_representation():
+    category = Category.objects.create(name="Work")
+    assert str(category) == "Work"
